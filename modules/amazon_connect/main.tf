b80ceb1b583af0830adc13ae114952fe5263872e
chendoh <72640611+chendoh@users.noreply.github.com>
2025-01-28 22:12:40 -0600

    destroy4

--- modules/amazon_connect/main.tf
+++ modules/amazon_connect/main.tf
@@ -7,23 +7,3 @@ resource "aws_connect_instance" "connect_instance" {
 
   tags = var.tags
 }
-
-
-# Amazon Connect Admin User
-resource "aws_connect_user" "admin_user" {
-  instance_id        = aws_connect_instance.connect_instance.id
-  hierarchy_group_id = null  # Set if using hierarchy groups
- ## routing_profile_id = aws_connect_routing_profile.admin_routing_profile.id
- ## security_profile_ids = [aws_connect_security_profile.admin_profile.id]
-  name                 = var.admin_user_name
-  password             = var.admin_user_password
-  identity_info {
-    first_name = var.admin_first_name
-    last_name  = var.admin_last_name
-  }
-  phone_config {
-    phone_type       = "SOFT_PHONE"
-    auto_accept      = true
-    after_contact_work_time_limit = 0
-  }
-}
