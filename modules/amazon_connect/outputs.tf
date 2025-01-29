4cdd395a60b56f1480749cacafb70cfaf6a4dc98
chendoh <72640611+chendoh@users.noreply.github.com>
2025-01-28 22:17:04 -0600

    destroy6

--- modules/amazon_connect/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "amazon_connect_instance_id" {
-  description = "Amazon Connect instance ID."
-  value       = aws_connect_instance.connect_instance.id
-}
-
-output "amazon_connect_instance_arn" {
-  description = "Amazon Connect instance ARN."
-  value       = aws_connect_instance.connect_instance.arn
-}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "amazon_connect_instance_id" {
-  description = "Amazon Connect instance ID."
-  value       = module.amazon_connect.amazon_connect_instance_id
-}
-
-output "amazon_connect_instance_arn" {
-  description = "Amazon Connect instance ARN."
-  value       = module.amazon_connect.amazon_connect_instance_arn
-}
