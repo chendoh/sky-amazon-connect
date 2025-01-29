b757dbe634c570462d30896f032e50bd6699a658
chendoh <72640611+chendoh@users.noreply.github.com>
2025-01-28 22:10:01 -0600

    destroy3

--- modules/amazon_connect/main.tf
+++ modules/amazon_connect/main.tf
@@ -13,8 +13,8 @@ resource "aws_connect_instance" "connect_instance" {
 resource "aws_connect_user" "admin_user" {
   instance_id        = aws_connect_instance.connect_instance.id
   hierarchy_group_id = null  # Set if using hierarchy groups
-  routing_profile_id = aws_connect_routing_profile.admin_routing_profile.id
-  security_profile_ids = [aws_connect_security_profile.admin_profile.id]
+ ## routing_profile_id = aws_connect_routing_profile.admin_routing_profile.id
+ ## security_profile_ids = [aws_connect_security_profile.admin_profile.id]
   name                 = var.admin_user_name
   password             = var.admin_user_password
   identity_info {
