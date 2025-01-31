eb213975125e1432190b9f30590235c40736e6e5
chendoh <72640611+chendoh@users.noreply.github.com>
2025-01-30 23:53:46 -0600

    cred 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 STACK_NAME="amazon-connect-stack"
-TEMPLATE_FILE="templates/amazon-connect-instance.yaml"
+TEMPLATE_FILE="templates/instance.yaml"
 REGION="us-east-1"
 
 echo "Deploying Amazon Connect Stack..."
 aws cloudformation deploy --stack-name $STACK_NAME --template-file $TEMPLATE_FILE --capabilities CAPABILITY_NAMED_IAM --region $REGION
+
+aws cloudformation deploy --stack-name $STACK_NAME --template-file $TEMPLATE_FILE --capabilities CAPABILITY_NAMED_IAM --region $REGION --parameter-overrides file://parameters.json
+
 
 echo "Deployment Completed!"
